0665bf4f0aecdaf525f490ceb199b794ab72b602
zoricavukovic <zok200015@gmail.com>
2022-04-04 16:50:24 +0200

    #1.1 Zavrsena pocetna inicijalizacija projekta

---
+++ BookingAppTeam05/src/main/java/com/example/BookingAppTeam05/BookingAppTeam05Application.java
@@ -0,0 +1,13 @@
+package com.example.BookingAppTeam05;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookingAppTeam05Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookingAppTeam05Application.class, args);
+	}
+
+}
---
+++ BookingAppTeam05/src/test/java/com/example/BookingAppTeam05/BookingAppTeam05ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.BookingAppTeam05;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookingAppTeam05ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
